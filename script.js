3def359310aaacbf50e9dde5b13b1c3517bf7752
kalyaniyelamanchili16 <kalyaniyelamanchili769@gmail.com>
2026-04-10 18:24:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Show welcome alert when page loads
+alert("Welcome to my portfolio!");
+
+// Function to display message
+function showMessage() {
+    alert("Thank you for visiting my portfolio!");
+}
+
+// Change background color
+function changeColor() {
+    document.body.style.backgroundColor = "#dfe6e9";
+}
